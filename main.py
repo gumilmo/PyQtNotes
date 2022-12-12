72163ec53079f69b283d66d6fba8c75c3c4b5264
gumilmo <gumilmo@yandex.ru>
2022-12-12 13:22:35 +0300

    resize but not for all images in field and bugs

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from PIL import Image
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QTextEdit, \
     QScrollArea, QPushButton, QDialog, QDialogButtonBox, QLabel, QMessageBox, QMenu
 from PyQt6.QtGui import QPalette, QColor, QCursor, QMouseEvent, QContextMenuEvent, QAction, QTextCursor, \
-    QTextImageFormat, QTextBlock, QTextFragment
+    QTextImageFormat, QTextBlock, QTextFragment, QResizeEvent
 from PyQt6.QtCore import Qt, QPoint, QEvent
 
 from services.services.text_edit_service import TextEditService
 from models.text_edit_field_model import TextEditField
 from models.textedit_layout import TextEditLayout
+from services.services.text_edit_service import TextEditService
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -21,65 +22,23 @@ class MainWindow(QMainWindow):
         self.setMinimumHeight(150)
         self.setMaximumHeight(620)
         self.setMaximumWidth(900)
-        #
-        # self.scroll_area = QScrollArea()
-        # self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        # self.scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        # self.scroll_area.setWidgetResizable(True)
-        # self.scroll_area.verticalScrollBar().setContentsMargins(10,10,10,10)
-        #
-        # self.text_edit_field = TextEditField()
-        #
-        # layout = QVBoxLayout()
-        # layout.setContentsMargins(10,20,10,20)
-        # layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-        #
-        # widget = QWidget()
-        #
-        # layout.addWidget(self.text_edit_field)
-        # widget.setLayout(layout)
-        # self.scroll_area.setWidget(widget)
-        #
-        # self.setCentralWidget(self.scroll_area)
 
         layout = TextEditLayout()
-        txt = TextEditField()
+        self.txt = TextEditField()
         widget = QWidget()
 
-
-        #doc = txt.document()
-        #cur = QTextCursor(doc)
-        #p1 = cur.position()
-        #cur.insertImage('cat2.png')
-
-        print("doc", layout.sizeHint().width())
-
-        it = txt.document().firstBlock().begin()
-        while not it.atEnd():
-            fragment = it.fragment()
-            if fragment.isValid():
-                if fragment.charFormat().isImageFormat():
-                    new_image_format = fragment.charFormat().toImageFormat()
-                    img = Image.open(new_image_format.name())
-                    width, height = img.size
-                    new_h = height * self.width() / width
-                    print('fffff')
-                    new_image_format.setWidth(self.width())
-                    new_image_format.setHeight(new_h)
-                    if new_image_format.isValid():
-                        txt_cur = txt.textCursor()
-                        txt_cur.setPosition(fragment.position())
-                        txt_cur.setPosition(fragment.position() + fragment.length(), QTextCursor.MoveMode.KeepAnchor)
-                        txt_cur.setCharFormat(new_image_format)
-            it += 1
+        self.text_edit_service = TextEditService()
 
         #fragment = it.fragment()
         #print(fragment)
         #print(it.fragment().length())
 
-        layout.addWidget(txt)
+        layout.addWidget(self.txt)
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        self.text_edit_service.resize_main(self.txt, self)
 
     # def mousePressEvent(self, a0) -> None:
     #     self.text_edit_field.textCursor()
--- services/services/text_edit_service.py
+++ services/services/text_edit_service.py
@@ -4,17 +4,40 @@ from PyQt6.QtGui import QPixmap, QFontMetrics, QFont
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QCheckBox, QComboBox, QListWidget, QLineEdit, QSpinBox, \
     QSlider, QDial, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QStackedLayout, QPushButton, QTabWidget, QTextEdit, \
     QScrollArea
-from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtGui import QPalette, QColor, QTextFragment, QTextDocument, QImage, QTransform, QTextCursor
 from services.interfaces.textedit_service_interface import TextEditServiceInterface
 
 
 class TextEditService(TextEditServiceInterface):
 
     @staticmethod
-    def resize_main(text_field) -> None:
-        print(text_field.toPlainText())
-        pass
-
+    def resize_main(text_field: QTextEdit, main_window: QMainWindow) -> None:
+        it = text_field.document().firstBlock().begin()
+        while not it.atEnd():
+            fragment = it.fragment()
+            #print(text_field.toPlainText())
+            if fragment.isValid():
+                for qurl in text_field.image_QUrls:
+                    image_resource: QImage = text_field.document().resource(
+                        QTextDocument.ResourceType.ImageResource,
+                        qurl
+                        )
+                    if fragment.charFormat().isImageFormat():
+                        new_image_format = fragment.charFormat().toImageFormat()
+                        print(qurl.fileName())
+                        #img = Image.open(new_image_format.name())
+                        width, height = image_resource.size().width(), image_resource.size().height()
+                        print(width, height)
+                        new_h = height * main_window.width() / width
+                    # print('fffff')
+                        new_image_format.setWidth(main_window.width())
+                        new_image_format.setHeight(new_h)
+                        if new_image_format.isValid():
+                            txt_cur = text_field.textCursor()
+                            txt_cur.setPosition(fragment.position())
+                            txt_cur.setPosition(fragment.position() + fragment.length(), QTextCursor.MoveMode.KeepAnchor)
+                            txt_cur.setCharFormat(new_image_format)
+            it += 1
     @staticmethod
     def resize_by_window(text_field) -> None:
         pass
